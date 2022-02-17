f61a35ae5f2f358f06dfba07a1e1982f62e259c3
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-17 14:00:19 +0200

    initiated back-end service

---
+++ back-end-service/src/test/java/com/electricity/seller/BackEndServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electricity.seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
