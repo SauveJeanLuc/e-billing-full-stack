ee151e404888696f688fc08a123482e921452c19
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-17 16:27:21 +0200

    Removed useless import

--- back-end-service/src/main/java/com/electricity/seller/repositories/IMeterRepository.java
+++ back-end-service/src/main/java/com/electricity/seller/repositories/IMeterRepository.java
@@ -1,9 +1,10 @@
 package com.electricity.seller.repositories;
 
 import com.electricity.seller.models.Meter;
-import com.electricity.seller.models.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IMeterRepository extends JpaRepository<Meter, Long> {
+import java.util.Optional;
 
+public interface IMeterRepository extends JpaRepository<Meter, Long> {
+    Optional<Meter> findByMeterNumber(Integer meterNumber);
 }
