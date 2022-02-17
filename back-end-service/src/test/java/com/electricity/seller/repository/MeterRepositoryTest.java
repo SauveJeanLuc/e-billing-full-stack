361f4952c45b15282520adeea90defca62e91be6
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-17 15:37:30 +0200

    Meter Repository ShouldSaveMeter Test passed

---
+++ back-end-service/src/test/java/com/electricity/seller/repository/MeterRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.electricity.seller.repository;
+
+
+import com.electricity.seller.models.Meter;
+import com.electricity.seller.repositories.IMeterRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(
+        properties = {
+                "spring.jpa.properties.javax.persistence.validation.mode=none"
+        }
+)
+public class MeterRepositoryTest {
+
+    @Autowired
+    private IMeterRepository underTest;
+
+    @Test
+    void itShouldSaveMeter() {
+        // Given
+        Long id = 132l;
+        Meter meter = new Meter(id, 123456);
+
+        // When
+         Meter savedMeter = underTest.save(meter);
+         assertThat(savedMeter).isEqualToComparingFieldByField(meter);
+
+    }
+
+}
