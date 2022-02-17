8fc7982059e5a4ee300ca4af67d9662ce74919da
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-17 15:18:09 +0200

    test Meter model Has Important Properties passed

---
+++ back-end-service/src/test/java/com/electricity/seller/models/MeterTest.java
@@ -0,0 +1,17 @@
+package com.electricity.seller.models;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertThat;
+
+public class MeterTest {
+
+    @Test
+    public void testMeterHasImportantProperties(){
+        Meter meter = new Meter();
+        assertThat(meter, hasProperty("id"));
+        assertThat(meter, hasProperty("meterNumber"));
+    }
+
+}
