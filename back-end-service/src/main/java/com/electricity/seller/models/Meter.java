867a09a3cb47e2d11c3aea3995eb2633dd6c8a4d
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-17 15:18:26 +0200

    Defined Meter Model

---
+++ back-end-service/src/main/java/com/electricity/seller/models/Meter.java
@@ -0,0 +1,27 @@
+package com.electricity.seller.models;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "meters")
+public class Meter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+
+    @Max(6)
+    @Min(6)
+    @Column(name = "meter_number", nullable = false, unique = true)
+    private Integer meterNumber;
+
+}
+
