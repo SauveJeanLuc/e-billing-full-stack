10e85aa640dfe45509b8c329dcab40f3eef58785
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-17 16:44:50 +0200

    defined BuyElectricityDTO

---
+++ back-end-service/src/main/java/com/electricity/seller/dtos/BuyElectricityDTO.java
@@ -0,0 +1,12 @@
+package com.electricity.seller.dtos;
+
+import lombok.Data;
+
+@Data
+public class BuyElectricityDTO {
+
+    private Integer amountOfMoney;
+
+    private Integer meterNumber;
+
+}
