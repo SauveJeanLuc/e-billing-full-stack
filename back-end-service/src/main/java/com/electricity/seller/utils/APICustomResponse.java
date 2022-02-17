b2af717c4aacf45a275ceda07195e14a718f68d0
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-17 16:29:22 +0200

    Refactored API Custom Response

--- back-end-service/src/main/java/com/electricity/seller/utils/APICustomResponse.java
+++ back-end-service/src/main/java/com/electricity/seller/utils/APICustomResponse.java
@@ -1,39 +1,18 @@
 package com.electricity.seller.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class APICustomResponse {
 
     private boolean status;
 
     private String message;
 
-
-    public APICustomResponse() {
-        super();
-    }
-
-
-    public APICustomResponse(boolean status, String message) {
-        super();
-        this.status = status;
-        this.message = message;
-    }
-
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    private Object data;
 
 
 }
