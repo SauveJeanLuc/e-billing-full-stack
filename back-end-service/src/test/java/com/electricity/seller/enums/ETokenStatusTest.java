f404f9c69c5c5938809a5471d284d57583e0bae0
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-17 15:11:50 +0200

    Test Enum Values passed

---
+++ back-end-service/src/test/java/com/electricity/seller/enums/ETokenStatusTest.java
@@ -0,0 +1,25 @@
+package com.electricity.seller.enums;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ETokenStatusTest {
+
+    @Test
+    public void testEnumValues(){
+        boolean ETokenStatusTestHasRequiredValues = false;
+        String expectedValue1 = "USED";
+        String expectedValue2 = "NOTUSED";
+
+        ETokenStatus values[] = ETokenStatus.values();
+
+        if( ! (expectedValue1.equals(values[0]) || (expectedValue2.equals(values[1]) ))){
+            ETokenStatusTestHasRequiredValues = true;
+        }
+
+        System.out.println(values[0]);
+        System.out.println(values[1]);
+
+        assertEquals(true, ETokenStatusTestHasRequiredValues, "EToken Status should have USED and NOTUSED as Values");
+    }
+}
