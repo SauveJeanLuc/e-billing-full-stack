92ee92c2aa7879c7c277915e3c9929c7d527dfec
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-20 15:50:16 +0200

    Marked IMeter and IToken as Repositories

--- back-end-service/src/main/java/com/electricity/seller/repositories/IMeterRepository.java
+++ back-end-service/src/main/java/com/electricity/seller/repositories/IMeterRepository.java
@@ -1,10 +1,13 @@
 package com.electricity.seller.repositories;
 
 import com.electricity.seller.models.Meter;
+import com.electricity.seller.models.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IMeterRepository extends JpaRepository<Meter, Long> {
     Optional<Meter> findByMeterNumber(Integer meterNumber);
 }
--- back-end-service/src/main/java/com/electricity/seller/repositories/ITokenRepository.java
+++ back-end-service/src/main/java/com/electricity/seller/repositories/ITokenRepository.java
@@ -2,7 +2,9 @@ package com.electricity.seller.repositories;
 
 import com.electricity.seller.models.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ITokenRepository extends JpaRepository<Token, Long> {
     boolean existsByValue(Integer value);
 }
