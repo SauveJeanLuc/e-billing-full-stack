61801bee21fa81b0564a0c9f4a61354936152c6f
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-20 16:09:43 +0200

    LoadElectricityDTOHasImportantProperties Test Passed

---
+++ back-end-service/src/test/java/com/electricity/seller/dtos/LoadElectricityDTOTest.java
@@ -0,0 +1,16 @@
+package com.electricity.seller.dtos;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertThat;
+
+public class LoadElectricityDTOTest {
+
+    @Test
+    public void testLoadElectricityDTOHasImportantProperties(){
+        LoadElectricityDTO dto = new LoadElectricityDTO();
+        assertThat(dto, hasProperty("token"));
+    }
+
+}
