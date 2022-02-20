b10c10ed23ef982859a12dd7db58480c60e36faa
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-20 16:09:57 +0200

    Defined LoadElectricityDTO

---
+++ back-end-service/src/main/java/com/electricity/seller/dtos/LoadElectricityDTO.java
@@ -0,0 +1,14 @@
+package com.electricity.seller.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoadElectricityDTO {
+
+    private Integer token;
+
+}
