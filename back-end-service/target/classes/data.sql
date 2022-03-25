806c8d872103ed07c4656aade883bd308495a47f
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-03-25 10:11:55 +0200

    push target

---
+++ back-end-service/target/classes/data.sql
@@ -0,0 +1,6 @@
+insert into meters(id, meter_number) values(3l, 123456);
+insert into meters(id, meter_number) values(4l, 123457);
+
+insert into tokens(id, value, duration, amount_payed, status, meter_id) values(3l, 12345687, 20, 2000, 'USED', 3l);
+
+
